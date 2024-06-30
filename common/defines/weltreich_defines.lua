023b52ce3d1ee3da84f9b32873de87f1fd448ca6
Weltreichmod <baizarremiller@gmail.com>
2024-06-30 17:25:44 +0900

    Weltreich main code
    Weltreich main code

---
+++ common/defines/weltreich_defines.lua
@@ -0,0 +1,56 @@
+NDefines.NGame.END_DATE = "1956.1.1.1"
+NDefines.NCharacter.DEFAULT_PP_COST_FOR_MILITARY_ADVISOR = 150
+NDefines.NDiplomacy.VOLUNTEERS_DIVISIONS_REQUIRED = 10
+
+NDefines.NCareerProfile.MOD_STATISTICS_GROUP = "weltreich_career_profile"
+NDefines.NCareerProfile.MOD_STATISTICS_GROUP_NAME = "WELTREICH_CAREER_PROFILE"
+	
+-- General AI
+NDefines.NAI.DIPLOMACY_FACTION_GLOBAL_TENSION_FACTOR = 0.1					-- Vanilla is 0.2
+NDefines.NAI.DIPLOMACY_FACTION_PLAYER_JOIN = 0								-- Vanilla is 20
+
+NDefines.NAI.DIPLOMACY_FACTION_NEUTRALITY_PENALTY = 20						-- Vanilla is 50
+
+NDefines.NAI.CALL_ALLY_NEUTRAL_DESIRE = 75									-- Vanilla is 25
+NDefines.NAI.JOIN_ALLY_NEUTRAL_DESIRE = 75									-- Vanilla is 25
+
+NDefines.NAI.FASCISTS_BEFRIEND_FASCISTS = 0									-- Vanilla is 10
+NDefines.NAI.FASCISTS_ALLY_FASCISTS = 0										-- Vanilla is 0
+NDefines.NAI.FASCISTS_ANTAGONIZE_FASCISTS = 0								-- Vanilla is -10
+
+NDefines.NAI.FASCISTS_BEFRIEND_DEMOCRACIES = 0								-- Vanilla is -25
+NDefines.NAI.FASCISTS_ALLY_DEMOCRACIES = 0									-- Vanilla is -100
+NDefines.NAI.FASCISTS_ANTAGONIZE_DEMOCRACIES = 0							-- Vanilla is 100
+
+NDefines.NAI.FASCISTS_BEFRIEND_COMMUNISTS = -25								-- Vanilla is -25
+NDefines.NAI.FASCISTS_ALLY_COMMUNISTS = -100								-- Vanilla is -100
+NDefines.NAI.FASCISTS_ANTAGONIZE_COMMUNISTS = 10							-- Vanilla is 100
+
+NDefines.NAI.DEMOCRACIES_BEFRIEND_FASCISTS = 0								-- Vanilla is -25
+NDefines.NAI.DEMOCRACIES_ALLY_FASCISTS = 0									-- Vanilla is -50
+NDefines.NAI.DEMOCRACIES_ANTAGONIZE_FASCISTS = 0							-- Vanilla is 0
+
+NDefines.NAI.DEMOCRACIES_BEFRIEND_DEMOCRACIES = 0							-- Vanilla is 0
+NDefines.NAI.DEMOCRACIES_ALLY_DEMOCRACIES = 0								-- Vanilla is 0
+NDefines.NAI.DEMOCRACIES_ANTAGONIZE_DEMOCRACIES = 0							-- Vanilla is -25
+
+NDefines.NAI.DEMOCRACIES_BEFRIEND_COMMUNISTS = 0							-- Vanilla is -25
+NDefines.NAI.DEMOCRACIES_ALLY_COMMUNISTS = 0								-- Vanilla is -50
+NDefines.NAI.DEMOCRACIES_ANTAGONIZE_COMMUNISTS = 0							-- Vanilla is 0
+
+NDefines.NAI.COMMUNISTS_BEFRIEND_FASCISTS = -25								-- Vanilla is -25
+NDefines.NAI.COMMUNISTS_ALLY_FASCISTS = -100								-- Vanilla is -100
+NDefines.NAI.COMMUNISTS_ANTAGONIZE_FASCISTS = 10							-- Vanilla is 100
+
+NDefines.NAI.COMMUNISTS_BEFRIEND_DEMOCRACIES = 0							-- Vanilla is -25
+NDefines.NAI.COMMUNISTS_ALLY_DEMOCRACIES = 0								-- Vanilla is -50
+NDefines.NAI.COMMUNISTS_ANTAGONIZE_DEMOCRACIES = 0							-- Vanilla is 10
+
+NDefines.NAI.COMMUNISTS_BEFRIEND_COMMUNISTS = 0							-- Vanilla is 25
+NDefines.NAI.COMMUNISTS_ALLY_COMMUNISTS = 0								-- Vanilla is 0
+NDefines.NAI.COMMUNISTS_ANTAGONIZE_COMMUNISTS = 0							-- Vanilla is -10
+
+NDefines.NAI.MAX_DISTANCE_NAVAL_INVASION = 150.0
+NDefines.NDiplomacy.IDEOLOGY_JOIN_FACTION_MIN_LEVEL = 0
+NDefines.NDiplomacy.MARKET_ACCESS_ACCEPTANCE_SAME_IDEOLOGY = 0				 -- Acceptance value added if same ideology (Vanilla is 15)
+NDefines.NDiplomacy.MARKET_ACCESS_ACCEPTANCE_TRADE_INFLUENCE = 0.2 			 -- Acceptance factor for trade influence (Vanilla is 0.70)
